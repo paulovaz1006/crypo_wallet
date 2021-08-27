0cd75aea6774ea2a68d682e2e832267572f4f3a7
Paulo Vaz <pvaz100@gmail.com>
2021-08-27 00:34:28 -0300

    feat: initial project

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'welcome/index'
+  #get '/inicio', to: 'welcome#index'
+
+  resources :coins
+  #get '/coins', to: 'coins#index'
+
+  root to: 'welcome#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
